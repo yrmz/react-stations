7a4396c99d7e8bc0a83d2c904899b2ad86691bda
Rimy <>
2021-06-08 22:33:35 +0900

    station3

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [img, setImg] = React.useState()
+
+  React.useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => setImg(data.message))
+  }, [])
+
   return (
     <div>
       <header>React Stations</header>
+      <div>犬の画像</div>
+      <img src={img} />
     </div>
   )
 }
