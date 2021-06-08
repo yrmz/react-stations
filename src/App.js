1eccd958dee8d615855ccf5a88594f59e800abd4
Rimy <>
2021-06-08 22:47:36 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setdogUrl] = React.useState(
+  const [dogUrl, setDogUrl] = React.useState(
     'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
   )
 
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(res => res.json())
-    .then(data => setdogUrl(data.message))
+  React.useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => setDogUrl(data.message))
+  }, [])
 
   return <img src={dogUrl} />
 }
